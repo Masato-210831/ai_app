d591f732ed3a88b5e507a11f4230134931c7150d
masato-210831 <b86500b@gmail.com>
2024-02-12 23:00:33 +0900

    chage nb.pickle from rdmf.pickle due to over volumns

--- nlp/views.py
+++ nlp/views.py
@@ -5,7 +5,7 @@ import pickle
 category_data = pd.read_csv("idx2category.csv")
 idx2category = {row.k: row.v for idx, row in category_data.iterrows()}
 
-with open('rdmf.pickle', mode="rb") as ff:
+with open('nb.pickle', mode="rb") as ff:
     model = pickle.load(ff)
 
 def index(request):
